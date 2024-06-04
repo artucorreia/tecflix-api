87f4a03d6be343d1aea09914fe00f8072d7a2291
artucorreia <115330684+artucorreia@users.noreply.github.com>
2024-06-03 21:52:36 -0300

    Revert "update"
    This reverts commit d897210000a2d9e47737de0b525587f8fdcca557.

--- src/main/java/com/artur/tecflix_api/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.artur.tecflix_api.repositories;
-
-import com.artur.tecflix_api.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
